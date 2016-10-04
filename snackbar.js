08915e8a6ac94508ce0868d2dbde9dfc6a670b75
didlich <didlich@t-online.de>
2016-10-04 11:19:12 +0200

    assume bower usage for template path

--- snackbar.js
+++ snackbar.js
@@ -4,7 +4,7 @@ angular.module("snackbar", ['ngAnimate']).service('$snackbar', function($http, $
   var timeout = {};
   var template = $http({
     method: 'GET',
-    url: 'lib/snackbar/snackbar.html'
+    url: 'bower_components/ion-snackbar/snackbar.html'
   }).then(function(result) {
     var body = document.getElementsByTagName("body")[0];
     var previousSnackbar = document.getElementsByClassName('snackbar-wrapper');
